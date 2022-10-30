cc699c736f638f2460ae2635bb8d38102f3f4ef8
poqpwppy <64719091+poqpwppy@users.noreply.github.com>
2022-10-31 02:39:54 +0700

    qhhue-api

---
+++ qhhue-api/js/bgmusic.js
@@ -0,0 +1,138 @@
+var music = document.getElementById("bgmusic");
+var time = document.getElementById("current-time");
+var durationContainer = document.getElementById("duration");
+var np = document.getElementById("song-name");
+var playhead = document.getElementById('playhead');
+var timeline = document.getElementById('timeline');
+
+var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+
+music.volume = 0.5;
+music.loop = false;
+
+var pbutton = document.getElementById("play-icon");
+var sbutton = document.getElementById("pause-icon");
+var nextb = document.getElementById("next-icon");
+var prevb = document.getElementById("prev-icon");
+var loopb = document.getElementById("loop-icon");
+
+pbutton.addEventListener("click", () => {
+  music.play();
+});
+
+sbutton.addEventListener("click", () => {
+  music.pause();
+});
+
+nextb.addEventListener("click", () => {
+  next();
+});
+
+prevb.addEventListener("click", () => {
+  prev();
+});
+
+loopb.addEventListener("click", () => {
+  if(music.loop === true) {
+    music.loop = false;
+    loopb.textContent = "Loop: Off";
+  } else {
+    music.loop = true;
+    loopb.textContent = "Loop: On";
+  }
+});
+
+const calculateTime = (secs) => {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  return `${minutes}:${returnedSeconds}`;
+}
+
+const displayTime = () => {
+  time.textContent = calculateTime(music.currentTime);
+}
+
+var duration;
+music.addEventListener("timeupdate", timeUpdate, false);
+
+function timeUpdate() {
+	var playPercent = 100 * (music.currentTime / duration);
+	playhead.style.marginLeft = playPercent + "%";
+  displayTime();
+}
+
+const displayDuration = () => {
+  durationContainer.textContent = calculateTime(music.duration);
+}
+
+if (music.readyState > 0) {
+  displayDuration();
+} else {
+  music.addEventListener('loadedmetadata', () => {
+    displayDuration();
+  });
+}
+
+music.addEventListener("canplaythrough", function () {
+	duration = music.duration;
+}, false);
+
+//Playlist functions
+var tracks = [
+  {"track":1,"name":"Về Những Ngày Đã Cũ... / 90's Session","src":"./music/venhungngaydacu.mp3"}
+];
+
+var i = 0;
+
+function next() {
+  if (i === tracks.length - 1) {
+      i = 0;
+  } else {
+      i++;
+  }
+
+  music.src = tracks[i].src;
+
+  np.textContent = tracks[i].name;
+
+  music.play();
+
+}
+
+if (music === null) {
+  throw "Playlist Player does not exists ...";
+} else {
+  music.src = tracks[i].src;
+
+  np.textContent = tracks[i].name;
+
+  music.play();
+
+}
+
+function prev() {
+  if (i === 0) {
+      i = tracks.length - 1;
+  } else {
+      i--;
+  }
+
+  music.src = tracks[i].src;
+
+  np.textContent = tracks[i].name;
+
+  music.play();
+}
+
+if (music === null) {
+  throw "Playlist Player does not exists ...";
+} else {
+  music.src = tracks[i].src;
+
+  np.textContent = tracks[i].name;
+
+  music.play();
+
+  music.addEventListener('ended', next, false)
+}
---
+++ qhhue-api/js/invbutton.js
@@ -0,0 +1,18 @@
+var inv = document.getElementById("srccode");
+var server = document.getElementById("sendpoqpwppy");
+var mainpage = document.getElementById("backmain");
+
+inv.addEventListener("click", () => {
+    window.open(
+        "https://github.com/poqpwppy/qhhue-api", "_blank");
+});
+
+server.addEventListener("click", () => {
+    window.open(
+        "https://www.facebook.com/poqpwppy/", "_blank");
+});
+
+mainpage.addEventListener("click", () => {
+    window.open(
+        "https://poqpwppy.github.io/");
+});
